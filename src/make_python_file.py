570779b118190ea1a4e8ba29a410f5d8755d5068
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-12-22 21:43:43 +1100

    create directory structure from json

--- src/make_python_file.py
+++ src/make_python_file.py
@@ -1,3 +1,4 @@
+import contextlib
 from pathlib import Path
 import json
 
@@ -25,10 +26,33 @@ def get_config(filename: str | Path, path: Path) -> dict:
     return config
 
 
+def make_directory_structure(file: dict, path: Path = get_project_root()):
+    """Make directory structure from JSON file.
+
+    Args:
+        file (dict): JSON file containing the directory structure to create.
+        path (Path, optional): Path where to create the directory. Defaults to get_project_root().
+    """
+    if file["type"] == "directory":
+        # Create the directory
+        print(f"Creating directory {path / file['name']}")
+        (path / file["name"]).mkdir(parents=True, exist_ok=True)
+
+        # Recursively process the children of the directory
+        with contextlib.suppress(KeyError):
+            for child in file["children"]:
+                make_directory_structure(child, path / file["name"])
+    else:
+        # Create the file
+        print(f"Creating file {path / file['name']}")
+        with (path / file["name"]).open("w", encoding="utf-8") as f:
+            f.write(" ")
+
+
 def main():
     settings = get_config("machine_learning.json", get_project_root() / "conf")
 
-    print(settings)
+    make_directory_structure(settings, Path("/workspaces/tmp"))
 
 
 if __name__ == "__main__":
