ae533b074f4eb970add8cfd2d4a8c68d110d3b6d
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2023-01-12 21:53:08 +1100

    update version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ docs_extras = [
 # This call to setup() does all the work
 setup(
     name="python.template",
-    version="0.2.2",
+    version="0.2.3",
     description="A cookiecutter template for Python projects",
     long_description=README,
     long_description_content_type="text/markdown",
